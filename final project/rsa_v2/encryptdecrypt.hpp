cb0f8c3306bb11660c4f06f83778a2478c430d1f
klandrith007@gmail.com <klandrith007@gmail.com>
2019-11-27 21:57:04 -0800

    added larger prime values and an external math library to handle them

--- final project/rsa_v2/encryptdecrypt.hpp
+++ final project/rsa_v2/encryptdecrypt.hpp
@@ -1,13 +1,16 @@
 
 /*  Programmer:     Kyle Landrith
     Date Competed:  11/27/19
-    Resources:      geeksforgeeks.com
-                    https://brilliant.org/wiki/rsa-encryption/
+    Resources:      https://brilliant.org/wiki/rsa-encryption/
                     https://simple.wikipedia.org/wiki/RSA_algorithm
                     https://don.p4ge.me/modular-exponentiation/programming
                     https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
     Description:    A class implementation and declaration to implement simple RSA
                     encryption.
+                    [note: prime numbers are originally stored in a string array
+                    because the ttmath library will not allow initializing integers
+                    to that large of a number, however it will support it, so it has
+                    to be assigned as a string and then converted]
 */
 
 #pragma once
@@ -16,11 +19,12 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include "ttmath/ttmath.h"
 
 using std::rand;
 using std::string;
 using std::stringstream;
-using std::hex;
+using std::stoi;
 using std::cout;
 
 class RSA {
@@ -28,18 +32,37 @@ public:
   // constructor
   RSA(unsigned int msgsize) {
     msglength = msgsize;
-    msg = new unsigned int[msglength];
-    encryptedmsg = new unsigned int[msglength];
+    msg = new ttmath::Int<64>[msglength];
+    encryptedmsg = new ttmath::Int<64>[msglength];
     decryptedmsg = new char[msglength];
+    for (int i = 0; i < 10; i++) {
+      stringstream stream;
+      stream << prime1string[i];
+      stream >> primenumbers1[i];
+    }
+    for (int i = 0; i < 5; i++) {
+      stringstream stream;
+      stream << prime2string[i];
+      stream >> primenumbers2[i];
+    }
   }
+
+  /*
+  // destructor
+  ~RSA() {
+    delete msg;
+    delete encryptedmsg;
+    delete decryptedmsg;
+  }
+  */
 
   // encryption function
   void encrypt(string message) {
     int index1, index2, eindex;
     // Seed the Random Number Generator
     srand(time(0));
-    index1 = rand() % 20;
-    index2 = rand() % 20;
+    index1 = rand() % 10;
+    index2 = rand() % 5;
     p = primenumbers1[index1];
     q = primenumbers2[index2];
     n = p * q;
@@ -64,9 +87,12 @@ public:
 
   // decryption function
   void decrypt() {
-    // decrypt the message and use type coercion to change from int to ASCII
     for (int i = 0; i < msglength; i++) {
-      decryptedmsg[i] = modExpo(encryptedmsg[i], d, n);
+      string str;
+      stringstream stream;
+      stream << modExpo(encryptedmsg[i], d, n);
+      str = stream.str();
+      decryptedmsg[i] = stol(str);
     }
   }
 
@@ -74,7 +100,7 @@ public:
   string getEncrypted() {
     stringstream stream;
     for (int i = 0; i < msglength; i++) {
-      stream << hex << encryptedmsg[i];
+      stream << encryptedmsg[i];
     }
     return stream.str();
   }
@@ -89,24 +115,34 @@ public:
   }
 
   // functions to return a string version of variables and keys
-  long long unsigned int getP() {
-    return p;
+  string getP() {
+    stringstream stream;
+    stream << p;
+    return stream.str();
   }
 
-  long long unsigned int getQ() {
-    return q;
+  string getQ() {
+    stringstream stream;
+    stream << q;
+    return stream.str();
   }
 
-  long long unsigned int getN() {
-    return n;
+  string getN() {
+    stringstream stream;
+    stream << n;
+    return stream.str();
   }
 
-  long long unsigned int getE() {
-    return e;
+  string getE() {
+    stringstream stream;
+    stream << e;
+    return stream.str();
   }
 
-  long long unsigned int getD() {
-    return d;
+  string getD() {
+    stringstream stream;
+    stream << d;
+    return stream.str();
   }
 
 private:
@@ -132,9 +168,9 @@ private:
     // end of loop [exp is 0]
     // return result [1]
     //
-    long long unsigned int modExpo(long long unsigned int base, long long unsigned int exp, long long unsigned int mod) {
-      long long unsigned int remain;
-      long long unsigned int result = 1;
+    ttmath::Int<64> modExpo(ttmath::Int<64> base, ttmath::Int<64> exp, ttmath::Int<64> mod) {
+      ttmath::Int<64> remain;
+      ttmath::Int<64> result = 1;
       while (exp != 0) {
         remain = exp % 2;
         exp = exp / 2;
@@ -148,9 +184,9 @@ private:
     // Iteratively uses the extended euclidean algorithm to
     // solve the following equation ax + by = gcd(a, b) for x
     // [note: assumes arguments passed are coprime]
-    long long unsigned int modInverse(long long unsigned int e, long long unsigned int phi) {
-      long long unsigned int phi0 = phi;
-      long long int y = 0, x = 1, quotient, temp;
+    ttmath::Int<64> modInverse(ttmath::Int<64> e, ttmath::Int<64> phi) {
+      ttmath::Int<64> phi0 = phi;
+      ttmath::Int<64> y = 0, x = 1, quotient, temp;
       if (phi == 1) return 0;
       while (e > 1) {
         quotient = e / phi;
@@ -171,25 +207,34 @@ private:
 
     // function to find the greatest common denominator of two integers
     // using euclides algorithm
-    long long unsigned int gcd(long long unsigned int a, long long unsigned int b) {
+    ttmath::Int<64> gcd(ttmath::Int<64> a, ttmath::Int<64> b) {
       if (b == 0) return a;
       return gcd(b, a % b);
     }
 
   // variables needed for encryption/decryption
-  long long unsigned int p, q, phi, n, e, d;
+  ttmath::Int<64> p, q, phi, n, e, d;
   int msglength;
-  unsigned int primenumbers1[20] = {14503, 14519, 14533, 14537, 14543, 14549,
-                                    14551, 14557, 14561, 14563, 14591, 14593,
-                                    14621, 14627, 14629, 14633, 14639, 14653,
-                                    14657, 14669};
-  unsigned int primenumbers2[20] = {14813, 14821, 14827, 14831, 14843, 14851,
-                                    14867, 14869, 14879, 14887, 14891, 14897,
-                                    14923, 14929, 14939, 14947, 14951, 14957,
-                                    14969, 14983};
-  int earray[20] = {7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31,
-                    33, 35, 37, 39, 41, 43, 45};
-  unsigned int *msg;
-  unsigned int *encryptedmsg;
+  ttmath::Int<64> primenumbers1[5];
+  ttmath::Int<64> primenumbers2[5];
+  string prime1string[10] = {"28911710017320205966167820725313234361535259163045867986277478145081076845846493521348693253530011243988160148063424837895971948244167867236923919506962312185829914482993478947657472351461336729641485069323635424692930278888923450060546465883490944265147851036817433970984747733020522259537",
+                            "16471581891701794764704009719057349996270239948993452268812975037240586099924712715366967486587417803753916334331355573776945238871512026832810626226164346328807407669366029926221415383560814338828449642265377822759768011406757061063524768140567867350208554439342320410551341675119078050953",
+                            "66906174941351898982237115390104837800016519944803542107267307151873963667405652882882234979074916465840726363291254831297728195876839948476821111362547660187546080833118811138386335112324867302804187267930262901208266565865718143732715527778968965624024419434824986425142476236837724711619",
+                            "17160441645030935264127918488197223044423032294949290607725329385702867211094124452026640439869290520138575924093242319478564624951322485066038129421782264744932574479313896348123684939555661072794352801320851822124548964799270331635834170197712511573673852876021092210734677851423038145589",
+                            "41881970328229837575862121024586873839118624339720906926151959380017384630908360261281114858896239995979802524451414999099221056661311384631724751694216776973306953252147904807235529083826421541134486578078657958631274409702333847706219977904901768889130257692466383613649686862454370409751",
+                            "12574310210568992295391236743812326726776482174353588657900645266285757770843033428320489364472989127456336702810628971254254492560827343895265298738632502255009593271051579723815454872757538904746185448795343318827589544795905106549913395621470148459136748084837312416788281030641376776461",
+                            "47636001527157843615510166362212813825553995166349359052560828063795751982743949356281378571281356684995526346349689115319412565735006540850254022130876377867983398374239198108049044484863543699346022227692817853215225730056764957039852693911896900286014520659611654434776741074401112976951",
+                            "36997438997453671132166865292569847244619444010395996997269402633033171733524207782461697123413414988859119764706460324782855919613943534861045216286685374559389860714010358938897470990143322775796082401132386180536650715835089633575578638914905606853502795766373129779823319637155282395679",
+                            "33224988939958325005500695671267617338874622885890390533881719663053642478645240639084719290104519985775137683284635121762491364078202654759655915865377054667244894109025593792728931763489582862581707296760107534128977579042328333430944033239749124630128178571199381307853292264015434330633",
+                            "66411949393245949268811711602826765576402057646975003006251042260813215340087318062380031915073315092183806206493533345953281647263552710655695269967337089850144857674638489475995919778552032603791816048265084649175429768094838031170157033168866305251844356097795408000548418783227194651709"};
+  string prime2string[5] = {"203956878356401977405765866929034577280193993314348263094772646453283062722701277632936616063144088173312372882677123879538709400158306567338328279154499698366071906766440037074217117805690872792848149112022286332144876183376326512083574821647933992961249917319836219304274280243803104015000563790123",
+                            "531872289054204184185084734375133399408303613982130856645299464930952178606045848877129147820387996428175564228204785846141207532462936339834139412401975338705794646595487324365194792822189473092273993580587964571659678084484152603881094176995594813302284232006001752128168901293560051833646881436219",
+                            "319705304701141539155720137200974664666792526059405792539680974929469783512821793995613718943171723765238853752439032835985158829038528214925658918372196742089464683960239919950882355844766055365179937610326127675178857306260955550407044463370239890187189750909036833976197804646589380690779463976173",
+                            "250556952327646214427246777488032351712139094643988394726193347352092526616305469220133287929222242315761834129196430398011844978805263868522770723615504744438638381670321613949280530254014602887707960375752016807510602846590492724216092721283154099469988532068424757856392563537802339735359978831013",
+                            "290245329165570025116016487217740287508837913295571609463914348778319654489118435855243301969001872061575755804802874062021927719647357060447135321577028929269578574760547268310055056867386875959045119093967972205124270441648450825188877095173754196346551952542599226295413057787340278528252358809329"};
+  int earray[20] = {9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33,
+                    35, 37, 39, 41, 43, 45, 47};
+  ttmath::Int<64> *msg;
+  ttmath::Int<64> *encryptedmsg;
   char *decryptedmsg;
 };
--- final project/rsa_v2/rsa.cpp
+++ final project/rsa_v2/rsa.cpp
@@ -1,18 +1,19 @@
 /*  Programmer:     Kyle Landrith
     Date Competed:  11/27/19
-    Resources:      geeksforgeeks.com
-                    https://brilliant.org/wiki/rsa-encryption/
+    Resources:      https://brilliant.org/wiki/rsa-encryption/
                     https://simple.wikipedia.org/wiki/RSA_algorithm
                     https://don.p4ge.me/modular-exponentiation/programming
                     https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
     Description:    A simple program to perform RSA encryption on a string message
                     entered through console input. Generates random keys each time
-                    the program is ran. May possibly adapt it in the future to work
-                    with file input and file output, as well as larger sized prime
-                    numbers for generating better keys.
+                    the program is ran (out of two sets of five 290 digit primes).
+                    Key strength is approximately 1924bit.
+                    May possibly adapt it in the future to work
+                    with file input and file output.
 */
 #include <iostream>
 #include <string>
+#include <unistd.h>
 #include "CinReader.h"
 #include "encryptdecrypt.hpp"
 
@@ -38,16 +39,18 @@ int main() {
     cout << "\nRandom prime numbers have been selected for use in\n"
          << "generating the public and private keys. \np: " << rsa.getP()
          << "\nq: " << rsa.getQ() <<"\n"
-         << "An odd number between 7 and 45 that is coprime to\n"
+         << "An odd number between 9 and 47 that is coprime to\n"
          << "(p - 1) * (q - 1) has been selected for the value of e.\n"
-         << "\nPublic key is: \n(n: " << rsa.getN() << ", e: " << rsa.getE()
-         << ")\n"
-         << "Private key is: \n(n: " << rsa.getN() << ", d: " << rsa.getD()
-         << ")\n";
+         << "Key length is approximately 1954 bits.\n"
+         << "\nPublic key is: \nn:\n" << rsa.getN() << "\ne:\n" << rsa.getE()
+         << "\n"
+         << "\nPrivate key is: \nn:\n" << rsa.getN() << "\nd:\n" << rsa.getD()
+         << "\n";
 
-    cout << "\nEncrypted messaged is [hexadecimal format]:\n" << rsa.getEncrypted() << endl;
+    cout << "\nEncrypted messaged is:\n" << rsa.getEncrypted() << endl;
+    cout << "\nDecrypting message..." << endl;
     rsa.decrypt();
-    cout << "\nDecrypted message is: \n" << rsa.getDecrypted() << endl;
+    cout << "\n\nDecrypted message is: \n" << rsa.getDecrypted() << endl;
     cout << "\n\nEnter 0 to exit, 1 to encrypt another message: ";
     selection = reader.readInt(0, 1);
   } while(selection == 1);
